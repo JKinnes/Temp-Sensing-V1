49ff6f02375aed561f679f7ec6850dc73caae5dd
JKinnes <joshua_kinnes@uri.edu>
2023-02-06 20:50:29 -0500

    Data and Alarms
    Fixed Serial data reading and added alarming checks using 2 variables

--- Temp-Sensing-V1.ino
+++ Temp-Sensing-V1.ino
@@ -2,18 +2,20 @@
 double temp[5];
 
 //Set pins for each temperature sensor
-int tempPin[5] = {2, 4, 6, 8, 10};
+int tempPin[5] = {0, 2, 4, 6, 8};
 
 //Set pins for alarming conditions
-double relay1 = 31; 
-double relay2 = 33;
-double alarm = 10;
+double relay1 = 39; 
+double relay2 = 41;
+double alarm = 12;
 
 //Define input and output word variable
-int inWord[32];
 double outWord[9];
-String data;
+String inWord;
 
+//Define strings for checking input word
+String zero = "0";
+String one = "1";
 
 void setup() {
   //Set up serial communication with Raspberry Pi
@@ -22,61 +24,62 @@ void setup() {
 
 void loop() {
 
-
   /*Check for serial data from Pi
     Input word to the Arduino is defined as follows:
     Character 1: State of power relays (1 or 0)
     Character 2: State of alarm (1 or 0)*/
-
   if (Serial.available() > 0){
-    String data = Serial.readStringUntil('\n');
+    inWord = Serial.readStringUntil('\n');
     Serial.print("You sent me: ");
-    Serial.println(data);
+    Serial.println(inWord);
 
-    if (data.charAt(1) == '1') {  //SERIAL OUTPUT ISSUE RESOLVED, SERIAL READ ISSUE REMAINS. BOARD SEES INPUT AND WRITES TO data STRING. READING data STRING IS CURRENT ISSUE
+  //Set relay activation voltages based on input word
+    if (inWord.startsWith(one)) {  //SERIAL OUTPUT ISSUE RESOLVED, SERIAL READ ISSUE REMAINS. BOARD SEES INPUT AND WRITES TO data STRING. READING data STRING IS CURRENT ISSUE
       digitalWrite(relay1, HIGH);
       digitalWrite(relay2, HIGH);
-      Serial.print("Got");
-    } else if (data.charAt(0) == 0) {
+    } else if (inWord.startsWith(zero)) {
       digitalWrite(relay1, LOW);
       digitalWrite(relay2, LOW);
     }
 
-    if (data.charAt(1) == 1) {
+  //Set alarm activation voltage based on input word
+    if (inWord.endsWith(one)) {
       digitalWrite(alarm, HIGH);
-    } else if (data.charAt(1) == 0) {
+    } else if (inWord.endsWith(zero)) {
       digitalWrite(alarm, LOW);
     } 
   }
-
-  /*int availableBytes = Serial.available();
-
-  for(int i = 0; i < availableBytes; i++) {
-    inWord[i] = Serial.read();
-  }
-  
-
-  //Set relay activation voltages based on input word
-  if (data.charAt(0) == 1) {
-    digitalWrite(relay1, HIGH);
-    digitalWrite(relay2, HIGH);
-    Serial.print("Got");
-  } else if (data.charAt(0) == 0) {
-    digitalWrite(relay1, LOW);
-    digitalWrite(relay2, LOW);
-  }
-  
-  //Set alarm activation voltage based on input word
-  if (data.charAt(1) == 1) {
-    digitalWrite(alarm, HIGH);
-  } else if (data.charAt(1) == 0) {
-    digitalWrite(alarm, LOW);
-  }*/
 
   //Read temp sensor voltages and set temp array floats
   //Round temp values to the nearest degree
   for (int i = 0; i < 5; i++) {
     temp[i] = (round((analogRead(tempPin[i]) / 1024) * 1000)/10);
+  }
+
+  //Define and reset warning variables;
+  int alarmWarn = 0;
+  int alarmTrigger = 0;
+
+  //Check if any temp sensors are 150 or over. Set alarm warning if true
+  for (int i = 0; i < 5; i++) {
+    if (temp[i] >= 150){
+      //Set alarmWarn to sensor over 150 to track for second check
+      alarmWarn = i;
+    }
+  }
+
+  //If second sensor is over 150, trigger alarm
+  if(alarmWarn != 0){
+    for (int i = 0; i < 5; i++) {
+     if (temp[i] >= 150 && i != alarmWarn){
+       alarmTrigger = 1;
+        //Remove relay activation voltage
+        digitalWrite(relay1, LOW);
+        digitalWrite(relay2, LOW);
+        //Apply buzzer voltage        
+        digitalWrite(alarm, HIGH);
+     }
+    }
   }
 
   //Begin writing data to output word
@@ -101,8 +104,7 @@ void loop() {
 
   outWord[8] = 4321;
 
-  /*
-    Send serial data from Arduino to Pi in several serial messages from outWord array
+  /*Send serial data from Arduino to Pi in several serial messages from outWord array
     Output word to the Arduino is defined as follows:
     Character 1: Word start character (1234)
     Character 2: State of power relays (1 or 0)
@@ -113,17 +115,10 @@ void loop() {
     Character 7: temp4 degrees
     Character 8: temp5 degrees
     Character 9: Word end character (4321)
-    Output word is printed and loop is repeated every 250ms
-  */
+    Output word is printed and loop is repeated every 250ms*/
   for (int i = 0; i < 9; i++){
   Serial.println(outWord[i]);
   }
-
-  //Serial.println("End");
-  //Serial.println(outWord[0]); 
-
-  /*outWord[1] = 1;
-  Serial.println(outWord[1]);*/
 
   delay(250);
 }
